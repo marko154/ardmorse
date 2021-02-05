5ed88a173768b4f60dad878e385c9b6b239b09ec
bine-cadez <78624233+bine-cadez@users.noreply.github.com>
2021-02-05 23:48:21 +0100

    Add files via upload

---
+++ server/client.js
@@ -0,0 +1,23 @@
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const WebSocket = require('ws');
+var socket = new WebSocket('ws://localhost:8080');
+
+const port = new SerialPort("COM3",{
+  baudRate:9600
+});
+
+const lineStream = port.pipe(new Readline({delimiter: '\r\n'}))
+lineStream.on('data', (stream) => {
+  console.log(stream);
+  socket.send(stream);
+});
+
+socket.addEventListener('open', function (event) {
+    console.log("connected");
+    socket.send("ready");
+});
+
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
