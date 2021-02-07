edcf10a577423f29dfa0f8bc3880f18e1861c02b
marko154 <marko.gartnar777gmail.com>
2021-02-07 18:53:08 +0100

    frontend improvements

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState,useEffect} from "react";
+import "./App.css";
+import { useState } from "react";
 import morsemap from "./morsemap";
-const socket = new WebSocket('ws://localhost:8079');
-const serverSocket = new WebSocket('ws://localhost:8080');
-
-
+import Button from "./Button/Button";
+import MorseCode from "./MorseCode/MorseCode";
+const socket = new WebSocket("ws://localhost:8079");
+const serverSocket = new WebSocket("ws://localhost:8080");
 
 function App() {
-   const [state, setState] = useState([]);
-   socket.addEventListener('message', function (event) {
-  if (!(event.data in morsemap)) {
-    setState(["pizda"]);
-  }
-  else {
-    setState([...state, morsemap[event.data]]);
+	const [letters, setLetters] = useState([]);
+	const [morseCode, setMorseCode] = useState([]);
 
-  }
-  });
+	socket.addEventListener("message", (event) => {
+		if (!(event.data in morsemap)) {
+			setLetters(["pizda"]);
+		} else {
+			setLetters([...letters, morsemap[event.data]]);
+			setMorseCode([...morseCode, event.data]);
+		}
+	});
 
-const handleAccept = (event) => {
-  if (state.length !== 0 && state[0] !=="pizda")
-  serverSocket.send(state[state.length - 1]);
-}
+	const handleAccept = () => {
+		if (letters.length !== 0 && letters[0] !== "pizda")
+			serverSocket.send(letters[letters.length - 1]);
+	};
 
-console.log(state);
-  return (
-    <div className="App">
-      <div className="text" style={{width:"50px", height:"50px",marginLeft:"50%",transform:"translateX(-50%)"}}>
-          <p> {state.join(" ")} </p>
-      </div>
-        <button onClick={handleAccept}>accept</button>
-        <button>discard</button>
-        <hr></hr>
-        <p>{"everything under 0.5 seconds is registered as dot"}</p>
-        <p>{"everything over 0.5 seconds is registered as dash"}</p>
-    </div>
-  );
+	const handleDiscard = () => {
+		if (letters.length > 0)
+			setLetters(letters.splice(0, letters.length - 1));
+	};
+
+	console.log(letters);
+	return (
+		<div className="App">
+			<div className="state">
+				<div className="word">
+					<p> {letters.join(" ")} </p>
+				</div>
+
+				<div className="current-letter">
+					<MorseCode morseCode={morseCode} />
+					<p>
+						{letters.length > 0
+							? letters[letters.length - 1]
+							: "Waiting"}
+					</p>
+				</div>
+			</div>
+
+			<Button onClick={handleAccept}>Accept</Button>
+			<Button onClick={handleDiscard}>Discard</Button>
+
+			<section>
+				<p>{"dot < 0.5 seconds"}</p>
+				<p>{"dash > 0.5 seconds"}</p>
+			</section>
+		</div>
+	);
 }
 
 export default App;
